d24f4ebb27b40c1a5b165b39ed389596e94ed7ca
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2024-06-16 15:22:44 +0530

    feat: wrapped off app bar

--- app/src/main/java/com/example/jetweatherapp/components/WeaatherAppBar.kt
+++ app/src/main/java/com/example/jetweatherapp/components/WeaatherAppBar.kt
@@ -1,13 +1,16 @@
 package com.example.jetweatherapp.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
@@ -31,7 +34,8 @@ fun WeatherAppBar(
     onAddActionClicked: () -> Unit = {},
     onButtonClicked: () -> Unit = {}
     ){
-    TopAppBar(title = {
+    TopAppBar(
+        title = {
 
         Text(text = title,
 
@@ -41,5 +45,26 @@ fun WeatherAppBar(
 
             ),
             )
-    })
+        },
+        actions = {
+              if(isMainScreen){
+                  IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+                  }
+                  IconButton(onClick = { /*TODO*/ }) {
+                      Icon(imageVector = Icons.Rounded.MoreVert, contentDescription = "More")
+                  }
+              }
+        },
+        navigationIcon = {
+                         if(icon != null){
+                             Icon(imageVector = icon, contentDescription = null,
+                                    modifier = Modifier.clickable {
+                                        onButtonClicked.invoke()
+                                    }
+                                 )
+                         }
+        },
+
+    )
 }
