b9cb35e568cc055f151360c842cf620f4cb781c8
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2024-06-22 11:36:56 +0530

    feat: added dao and data class

---
+++ app/src/main/java/com/example/jetweatherapp/data/WeatherDao.kt
@@ -0,0 +1,30 @@
+package com.example.jetweatherapp.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.jetweatherapp.model.Favorite
+import kotlinx.coroutines.flow.Flow
+import java.util.UUID
+
+@Dao
+interface WeatherDao {
+    @Query("SELECT * from favorite_tbl")
+    fun getFavorites(): Flow<List<Favorite>>
+    @Query("SELECT * from favorite_tbl where city = :city")
+    suspend fun getFavByCity(city: String): Favorite
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavorite(favorite: Favorite)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateFavorite(favorite: Favorite)
+
+    @Query("DELETE from favorite_tbl")
+    suspend fun deleteAllFavorite(favorite: Favorite)
+
+    @Delete
+    suspend fun deleteFavorite(favorite: Favorite)
+}
---
+++ app/src/main/java/com/example/jetweatherapp/model/Favorite.kt
@@ -0,0 +1,17 @@
+package com.example.jetweatherapp.model
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity(tableName = "favorite_tbl")
+data class Favorite(
+    @PrimaryKey
+    val id: UUID,
+    @ColumnInfo(name = "city")
+    val city: String,
+    @ColumnInfo(name = "country")
+    val country: String
+)
